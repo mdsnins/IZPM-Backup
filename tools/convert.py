78874311b804e06f4e509e4e14fa9c1630718bd7
Jihwan Kim <payload@kaist.ac.kr>
2021-05-12 17:35:48 +0900

    Add simplifing script

---
+++ tools/convert.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8 -*-
+import json
+import sys
+
+def main():
+	if len(sys.argv) != 2:
+		return
+
+	f = open(sys.argv[1], "r", encoding="UTF-8")
+	raw = f.read()[14:]
+	pm_data = json.loads(raw)
+	
+	for pm in pm_data:
+		pm.pop("subject")
+		pm.pop("preview")
+
+	f = open("pm_upload.json", "w", encoding="UTF-8")
+	f.write(json.dumps(pm_data))
+	f.close()
+
+if __name__ == "__main__":
+	main()
